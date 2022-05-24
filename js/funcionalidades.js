64838cf657cc64371f0f60d2b5d5422a6061ecce
Javier <jpizarroj01@educarex.es>
2022-05-24 22:15:20 +0200

    asd

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -1,31 +1,3 @@
-function inicio(){
-    document.getElementById("descrip").style.display = "none";
-    document.getElementById("princ").style.display = "flex";
-    document.getElementById("enlac").style.display = "flex";
-    document.getElementById("clas").style.display = "none";
-    document.getElementById("gal").style.display = "none";
-}
-function descripcion(){
-    document.getElementById("descrip").style.display = "flex";
-    document.getElementById("princ").style.display = "none";
-    document.getElementById("enlac").style.display = "none";
-    document.getElementById("clas").style.display = "none";
-    document.getElementById("gal").style.display = "none";
-}
-function clases(){
-    document.getElementById("descrip").style.display = "none";
-    document.getElementById("princ").style.display = "none";
-    document.getElementById("enlac").style.display = "none";
-    document.getElementById("clas").style.display = "flex";
-    document.getElementById("gal").style.display = "none";
-}
-function galeria(){
-    document.getElementById("descrip").style.display = "none";
-    document.getElementById("princ").style.display = "none";
-    document.getElementById("enlac").style.display = "none";
-    document.getElementById("clas").style.display = "none";
-    document.getElementById("gal").style.display = "grid";
-}
 function formulario_enviado(){
     var nombre_persona = document.getElementById("nombre").value;
     var apellido_persona = document.getElementById("apellidos").value;
