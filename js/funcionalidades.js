01e57a8be4e29a151ea53cf229586afe7ee56bb2
jpizarroj01 <jpizarroj01@educarex.es>
2022-05-25 12:11:51 +0200

    100%

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -2,12 +2,16 @@ let usuario_;
 document.getElementById("name").innerHTML = usuario_;
 function formulario_enviado(){
     var nombre_persona = document.getElementById("nombre").value;
-    var nombre_persona = document.getElementById("apellidos").value;
+    var apellido_persona = document.getElementById("apellidos").value;
     var contrasena_persona = document.getElementById("password").value;
-    if (nombre_persona == "" || apellido_persona == "" || contrasena_persona.length < 8 || contrasena_persona.length > 12) {
-        alert("Escriba los campos obligatorio como se indican");
-     } else
-     alert("Gracias por contactar con nosotros " + nombre_persona + " " + apellido_persona + ", procederemos a revisar el problema, mientras puede seguir navegando por la web");
+    if (nombre_persona.length > 0){
+        if(apellido_persona.length > 0){
+            if (contrasena_persona.length >= 8 && contrasena_persona.length <= 12){
+                alert("Gracias por contactar con nosotros " + nombre_persona + " " + apellido_persona + ", procederemos a revisar el problema, mientras puede seguir navegando por la web");
+            }
+        }
+    }
+    else alert("Error, rellene los campos")
 }
 function usuario(){
     usuario_ = prompt("Escriba su nombre");
