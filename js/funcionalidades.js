1d9fc3eed30700526741461e03590bdb31d126af
jpizarroj01 <jpizarroj01@educarex.es>
2022-05-25 10:01:36 +0200

    100%

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -1,5 +1,10 @@
 function formulario_enviado(){
     var nombre_persona = document.getElementById("nombre").value;
     var apellido_persona = document.getElementById("apellidos").value;
-    alert("Gracias por contactar con nosotros " + nombre_persona + " " + apellido_persona + ", procederemos a revisar el problema, mientras puede seguir navegando por la web");
+    var contrasena_persona = document.getElementById("password").value;
+    if (nombre_persona == "" || apellido_persona == "" || contrasena_persona.length < 8 || contrasena_persona.length > 12) {
+        alert("Escriba los campos obligatorio como se indican");
+     } else
+     alert("Gracias por contactar con nosotros " + nombre_persona + " " + apellido_persona + ", procederemos a revisar el problema, mientras puede seguir navegando por la web");
+    
 }
